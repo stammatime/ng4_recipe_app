c0ccae947f8b4c701fd7f14ee7d2d41bf1ff282b
Cody <stammer.3@osu.edu>
2017-11-19 01:46:10 -0500

    updated recipe to have ingredients and display them

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,19 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from 'app/shared/ingredient.model';
 export class RecipeService {
 
     private recipes: Recipe[] = [
-        new Recipe ("Beef Wellington","A local delicacy","https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Mitarashi_dango_by_denver935.jpg/1200px-Mitarashi_dango_by_denver935.jpg"),
-        new Recipe ("Dango","A delicious dish","http://wppullzone1.epicmatcha.netdna-cdn.com/wp-content/uploads/2016/03/matcha-cookies-hanami-dango.jpg")
+        new Recipe ("Beef Wellington","A local delicacy","https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Mitarashi_dango_by_denver935.jpg/1200px-Mitarashi_dango_by_denver935.jpg", 
+        [
+            new Ingredient("Beef", 1),
+            new Ingredient ("Wellington", 1)
+        ]),
+        new Recipe ("Dango","A delicious dish","http://wppullzone1.epicmatcha.netdna-cdn.com/wp-content/uploads/2016/03/matcha-cookies-hanami-dango.jpg", 
+        [ 
+            new Ingredient("Dough ball", 4),
+            new Ingredient ("Skewer", 1)
+        ])
       ];
 
       recipeSelected = new EventEmitter<Recipe>();
