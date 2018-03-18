01be10e93b28858715b90685292e7424773904ea
Cody <stammer.3@osu.edu>
2018-03-18 01:12:36 -0400

    Added shopping list edit ngForm and update functionality

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,12 +1,17 @@
 import { Ingredient} from '../shared/ingredient.model';
 import { Subject } from 'rxjs/Subject';
 export class ShoppingListService {
+    startedEditing = new Subject<number>();
     ingredients: Ingredient[] = [
         new Ingredient("Dough", 5),
         new Ingredient("Stick", 1)
       ];
 
       ingredientAdded = new Subject<Ingredient[]>();
+
+      getIngredient(index: number) {
+        return this.ingredients[index];
+      }
 
       getIngredients(){
         return this.ingredients.slice();
@@ -20,5 +25,15 @@ export class ShoppingListService {
       addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients); // spread operator turns ingredients into list instead of array
         this.ingredientAdded.next(this.ingredients.slice());        
+      }
+
+      updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientAdded.next(this.ingredients.slice());
+      }
+
+      deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
+        this.ingredientAdded.next(this.ingredients.slice());
       }
 }
